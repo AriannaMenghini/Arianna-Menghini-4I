9a21bc200fc5782a779d2c0bb853c4d2a05a89dc
AriannaMenghini <arianna.menghini@gmail.com>
2020-10-23 22:03:21 +0200

    stampa_stringa_al_contrario.c

---
+++ stampa_stringa_al_contrario.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+void stampa(char* s); // char[]
+void stampaRev(char* s); // char[]
+
+/*
+    command:    ./main pippo pluto
+    argv:
+    x    0 1 2 3 4 5 6 7 8
+    0    . / m a i n \0
+    1    p i p p o \0
+    2    p l u t o \0
+*/
+
+
+int main(int argc, char** argv) { // char[][]
+    int i;
+
+    printf("%d\n", argc);
+    
+    if (argc != 1) 
+        printf("\nStringa inserita: \n");
+
+    for(i=0; i<argc; i++){
+        stampa(*(argv+i)); // argv[i]
+
+        printf("\n");
+    }
+
+    if (argc != 1) 
+        printf("\nStringa inserita, al contrario: \n");
+
+    for(i=0; i<argc; i++){
+        stampaRev(*(argv+i)); // argv[i]
+
+        printf("\n");
+    }
+}
+
+void stampa(char* s){
+    if(*s!='\0'){
+        printf("%c",*s);
+        stampa(s+1);
+    }
+}
+
+
+void stampaRev(char* s){
+    if(*s!='\0'){
+        stampa(s+1);
+        printf("%c",*s);
+    }
+}
